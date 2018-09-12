57906cc49949b737ce4b286a7c2ef307b732a7a3
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2018-09-12 07:29:19 -0500

    Update notation.js

--- notation.js
+++ notation.js
@@ -6,5 +6,6 @@ function format(){
     
     var exponet = length - 1
     
-    alert(number + " * " + "10^" + exponet)
+    alert(number/(10^exponet) + " * " + "10^" + exponet)
+    //1526000000 to 1256 * 10^9
 }
